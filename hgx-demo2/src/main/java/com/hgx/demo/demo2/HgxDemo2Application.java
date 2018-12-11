e0d3fa5a2640b298d8690746aeedfb91dd31e960
hgx61 <hgx615@163.com>
2018-12-11 14:24:45 +0800

    hgx-demo2

---
+++ hgx-demo2/src/main/java/com/hgx/demo/demo2/HgxDemo2Application.java
@@ -0,0 +1,12 @@
+package com.hgx.demo.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgxDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HgxDemo2Application.class, args);
+    }
+}
